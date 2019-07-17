a07c0b30f5c0f996fe78cb023d76f082d08c9fc0
EvgeniyChaika <evgeniy.chaika.83@gmail.com>
2019-07-18 00:38:18 +0300

    Web security templates

--- src/main/java/jwt/auth/TestResource.java
+++ src/main/java/jwt/auth/TestResource.java
@@ -4,12 +4,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 /**
  * Created by echaika on 17.07.2019
  */
 @RestController
 @RequestMapping("/rest/test")
 public class TestResource {
+
+    @GetMapping("/principal")
+    public Principal principal(Principal principal) {
+        return principal;
+    }
 
     @GetMapping
     public String test() {
--- src/main/java/jwt/auth/config/ResourceServerConfig.java
+++ src/main/java/jwt/auth/config/ResourceServerConfig.java
@@ -1,8 +1,10 @@
 package jwt.auth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -41,6 +43,12 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                 .inMemoryAuthentication()
                 .withUser("echaika")
                 .password("echaika")
-                .roles("admin");
+                .roles("USER");
+    }
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
